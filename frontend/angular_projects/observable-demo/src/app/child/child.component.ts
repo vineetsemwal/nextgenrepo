7525cf716c58f7264d8b84a7624cf63019aa1e41
vineetsemwal <vineetsemwal82@gmail.com>
2022-01-13 10:23:03 +0530

    another way of subscribing

--- frontend/angular_projects/observable-demo/src/app/child/child.component.ts
+++ frontend/angular_projects/observable-demo/src/app/child/child.component.ts
@@ -1,23 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'child',
   templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css']
+  styleUrls: ['./child.component.css'],
 })
 export class ChildComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  addResult:number|undefined;
+  addResult: number | undefined;
 
   ngOnInit(): void {
-    console.log("task assigned");  
-   // let obs:Observable<number>=this.add(10,20);
-   let obs:Observable<number>=this.add(undefined,undefined);
-   console.log("about to subscribe")
-    obs.subscribe(
+    console.log('task assigned');
+    // let obs:Observable<number>=this.add(10,20);
+    let obs: Observable<number> = this.add(undefined, undefined);
+    console.log('about to subscribe');
+
+    /* obs.subscribe(
           (result:number)=>{
            this.addResult=result;
            console.log("result received",result);
@@ -26,23 +26,36 @@ export class ChildComponent implements OnInit {
             console.log("received err",err);
           }
       );
+*/
 
-     console.log("subscribed"); 
-      
-  }
-
-  
-  add(num1:number, num2:number):Observable<number>{
-  let obs:Observable<number>=new Observable<number>(subscriber=>{
-       if(!num1 ||!num2){
-         subscriber.error(new Error("num1 or num2 is invalid"));
+    const observer:Observer<number> = {
+      next: (result: number) => {
+        this.addResult = result;
+        console.log('result received', result);
+      },
+      error: (err) => {
+        console.log('received err', err);
+      },
+      complete:()=>{
+        console.log("completed" );
       }
-      let sum=num1+num2;
-      console.log("about to publish result");
-      subscriber.next(sum);  
-      console.log("result already published");
-  });
-  return obs;
+    };
+
+    obs.subscribe(observer);
+
+    console.log('subscribed');
   }
 
+  add(num1: number, num2: number): Observable<number> {
+    let obs: Observable<number> = new Observable<number>((subscriber) => {
+      if (!num1 || !num2) {
+        subscriber.error(new Error('num1 or num2 is invalid'));
+      }
+      let sum = num1 + num2;
+      console.log('about to publish result');
+      subscriber.next(sum);
+      console.log('result already published');
+    });
+    return obs;
+  }
 }
