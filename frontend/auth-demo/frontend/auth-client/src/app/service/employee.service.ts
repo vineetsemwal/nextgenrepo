5c60e7ab8442a94f6c42d17ac8eac47062ef2c4e
vineetsemwal <vineetsemwal82@gmail.com>
2022-01-18 10:19:12 +0530

    employeeservice improved, doesnt handle token

--- frontend/auth-demo/frontend/auth-client/src/app/service/employee.service.ts
+++ frontend/auth-demo/frontend/auth-client/src/app/service/employee.service.ts
@@ -13,13 +13,8 @@ export class EmployeeService {
   constructor(private client:HttpClient, private authService:AuthService) { }
 
   fetchEmployeeByUsername(username:string):Observable<Employee>{
-    const token:string|null=this.authService.getToken();
-    if(token==null){
-      return new Observable(subscriber=>subscriber.error(new Error("user not logged in")));
-    }
-    const url=baseUrl+"/employees/byusername/"+username ;
-    //const options={headers:{"token":token}};
-    const obs:Observable<Employee>=this.client.get<Employee>(url);
+      const url=baseUrl+"/employees/byusername/"+username ;
+     const obs:Observable<Employee>=this.client.get<Employee>(url);
     return obs;
   }
 
