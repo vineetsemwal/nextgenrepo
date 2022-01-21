aa684c6bcc86771cfc4f124fb2c46b9e6742e8bb
vineetsemwal <vineetsemwal82@gmail.com>
2022-01-21 10:05:16 +0530

    map demo

--- serverside/javaexperiments/src/collectiondemos/listex/ArrayListEx.java
+++ serverside/javaexperiments/src/collectiondemos/listex/ArrayListEx.java
@@ -1,6 +1,7 @@
 package collectiondemos.listex;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
---
+++ serverside/javaexperiments/src/mapex/MapEx1.java
@@ -0,0 +1,49 @@
+package mapex;
+
+import emp.Employee;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MapEx1 {
+    public static void main(String[] args){
+       //keys are unique, values can be duplicated
+        Map<Integer, Employee>map=new HashMap<>();
+        Employee emp1=new Employee(1,"rishwanth",(byte)25);
+        Employee emp2=new Employee(2,"ramanna",(byte)25);
+        Employee emp3=new Employee(3,"vinay",(byte)25);
+        map.put(3,emp3);
+        map.put(1,emp1);
+        map.put(2,emp2);
+        map.put(2,emp2);
+        int size=map.size();
+        System.out.println("size of map="+size);
+        Employee fetchedForKey1=map.get(1);
+        System.out.println("fetched for key 1 , value="+fetchedForKey1);
+        System.out.println("iterating on keys below");
+        Set<Integer> keys= map.keySet();
+       for (Integer key:keys){
+          Employee iterated= map.get(key);
+           System.out.println("key="+key+" value="+iterated);
+       }
+       map.remove(1);
+        System.out.println("rmoving entry with key 1");
+       size=map.size();
+        System.out.println("map size after removing entry= "+size);
+        System.out.println("iterating on values below");
+        Collection<Employee> values=map.values();
+       for(Employee iterated:values){
+           System.out.println("value "+iterated);
+       }
+        System.out.println("iterating on entries below");
+      Set<Map.Entry<Integer,Employee>>entries= map.entrySet();
+      for(Map.Entry<Integer,Employee> entry:entries){
+          Integer key=entry.getKey();
+          Employee value=entry.getValue();
+          System.out.println("key="+key+"value="+value);
+      }
+
+    }
+}
