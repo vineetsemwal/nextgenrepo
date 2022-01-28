41f0c14644f784323e68a3be6ebb5cec4ccbbcd6
vineetsemwal <vineetsemwal82@gmail.com>
2022-01-28 10:27:02 +0530

    update for previous commit, one to one single directional demo

---
+++ serverside/jpa/associations/ontoone/singledirectional/src/main/java/com/mycompany/singledirectional/Demo.java
@@ -0,0 +1,66 @@
+package com.mycompany.singledirectional;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.function.Consumer;
+
+public class Demo {
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("empms");
+    private EntityManager em = emf.createEntityManager();
+
+
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+        demo.start();
+    }
+
+    public void start() {
+
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+        Address address=new Address();
+        address.setCity("mumbai");
+        address.setState("maha");
+        address.setFlatNumber("201 B");
+        em.persist(address);
+
+        Employee employee=new Employee();
+        employee.setName("rajiv");
+        employee.setAge(28);
+        employee.setAddress(address);
+
+
+        em.persist(employee);
+
+        transaction.commit();
+        System.out.println("employee inserted");
+
+        displayEmployee(employee);
+
+
+        em.close();
+        emf.close();
+    }
+
+
+
+    public void displayEmployee(Employee emp) {
+        System.out.println("emp-" + emp.getId() + "-" + emp.getName() + "-" + emp.getAge());
+        Address address=emp.getAddress();
+        displayAddress(address);
+    }
+
+    public void displayAddress(Address address){
+        System.out.println("address-"+address.getId()+"-"+address.getCity()+"-"+address.getState()+"-"+address.getFlatNumber());
+
+    }
+
+
+
+    void displayCollection(Collection<Employee> collection) {
+        Consumer<Employee> consumer = this::displayEmployee;
+        collection.stream().forEach(consumer);
+    }
+
+}
---
+++ serverside/jpa/associations/ontoone/singledirectional/src/main/java/com/mycompany/singledirectional/Employee.java
@@ -0,0 +1,72 @@
+package com.mycompany.singledirectional;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Table(name="employees_data")
+@Entity
+public class Employee {
+    @GeneratedValue
+    @Id
+    private Long id;
+    @Column(name="empname",nullable=false)
+    private String name;
+
+    @Column(nullable=false)
+    private int age;
+
+    @OneToOne
+    private Address address;
+
+    public Employee() {}
+
+    public Employee(String name, int age){
+        setName(name);
+        setAge(age);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id.equals(employee.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
