2752b7bbc2e5cf2fa08d2cd4c2b4075141d7c14c
vineetsemwal <vineetsemwal82@gmail.com>
2022-01-29 12:22:45 +0530

    minor improvement in spring-jpa demo + also committing changes dure to directory move

--- serverside/jee/spring/devmanagedtransaction/empms/src/main/java/com/mycompany/empms/App.java
+++ serverside/jee/spring/devmanagedtransaction/empms/src/main/java/com/mycompany/empms/App.java
@@ -1,14 +1,28 @@
 package com.mycompany.empms;
 
 import com.mycompany.empms.frontend.FrontEndClient;
+import com.mycompany.empms.util.JpaUtil;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 
 public class App {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         FrontEndClient frontEnd = context.getBean(FrontEndClient.class);
         frontEnd.render();
+
+        JpaUtil jpaUtil = context.getBean(JpaUtil.class);
+        EntityManager em = jpaUtil.getEntityManager();
+        if (em != null) {
+            em.close();
+        }
+        EntityManagerFactory emf = jpaUtil.getEntityManagerFactory();
+        if (emf != null) {
+            emf.close();
+        }
         context.close();
 
 
--- serverside/jee/spring/devmanagedtransaction/empms/src/main/java/com/mycompany/empms/util/JpaUtil.java
+++ serverside/jee/spring/devmanagedtransaction/empms/src/main/java/com/mycompany/empms/util/JpaUtil.java
@@ -9,16 +9,24 @@ import javax.persistence.Persistence;
 @Component
 public class JpaUtil {
 
+    private EntityManagerFactory entityManagerFactory;
     private EntityManager em;
 
-    public EntityManager getOrCreateEntityManager(){
-         if(em!=null){
-             return em;
-         }
+    public EntityManager getOrCreateEntityManager() {
+        if (em != null) {
+            return em;
+        }
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("empms");
-         em = emf.createEntityManager();
-         return em;
+        em = emf.createEntityManager();
+        return em;
     }
 
+    public EntityManager getEntityManager() {
+        return em;
+    }
+
+    public EntityManagerFactory getEntityManagerFactory() {
+        return entityManagerFactory;
+    }
 
 }
